dfc2bb5f27be57857165fa81ac41aa83ce9ef457
sounderya <sounderyarameshbabu@sounderyas-macbook-pro.local>
2016-01-28 08:56:15 +0800

    +2 static pages for user

--- CarFix/ViewController.swift
+++ CarFix/ViewController.swift
@@ -8,15 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+     
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         view.addGestureRecognizer(tap)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var textView: UITextView!
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    
+        //textView.setContentOffset(CGPointMake(0.0, 0.0), animated: false)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
