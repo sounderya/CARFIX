2b79a48af06dfe48f11b05c4288c2f401566e767
sounderya <sounderyarameshbabu@sounderyas-macbook-pro.local>
2016-01-28 23:38:46 +0800

    Allignment+color variations

--- CarFix/AppDelegate.swift
+++ CarFix/AppDelegate.swift
@@ -17,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().backgroundColor = UIColor.redColor()
+        UINavigationBar.appearance().barTintColor = UIColor.redColor()
+        UITabBar.appearance().backgroundColor = UIColor.redColor()
+        UITabBar.appearance().barTintColor = UIColor.redColor()
+        UIBarButtonItem.appearance().tintColor = UIColor.blackColor()
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+
         return true
     }
 
