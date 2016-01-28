bcb77480a99aaabd2cbdbee70d7995b31940a468
sounderya <sounderyarameshbabu@sounderyas-macbook-pro.local>
2016-01-28 12:46:46 +0800

    Minor Alignment issues still pending

---
+++ CarFix/CreateDriverProfileVC.swift
@@ -0,0 +1,71 @@
+//
+//  CreateDriverProfileVC.swift
+//  CarFixUI
+//
+//  Created by Sounderya Ramesh Babu on 20/1/16.
+//  Copyright © 2016 CarFix. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CreateDriverProfileVC:UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+    let imagePicker = UIImagePickerController()
+    @IBOutlet weak var imageview: UIImageView!
+    
+    
+    @IBAction func selectPhotoButtonTapped(sender: AnyObject) {
+        
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        presentViewController(picker, animated: true, completion: nil)
+        
+    }
+    
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        var newImage: UIImage
+        
+        if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            newImage = possibleImage
+            
+            imageview.image = possibleImage
+        } else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            newImage = possibleImage
+            
+            imageview.image = possibleImage
+        } else {
+            return
+        }
+        let imageName = NSUUID().UUIDString
+        let imagePath = getDocumentsDirectory().stringByAppendingPathComponent(imageName)
+        
+        if let jpegData = UIImageJPEGRepresentation(newImage, 80) {
+            jpegData.writeToFile(imagePath, atomically: true)
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func getDocumentsDirectory() -> NSString {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    
+}
