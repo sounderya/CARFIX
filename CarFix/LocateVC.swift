35c2e26916f707a9600551fa8f698cb55d99c654
sounderya <sounderyarameshbabu@sounderyas-macbook-pro.local>
2016-02-02 09:49:24 +0800

    empty page added for redirection if policy is valid.

---
+++ CarFix/LocateVC.swift
@@ -0,0 +1,20 @@
+//
+//  LocateVC.swift
+//  CarFix
+//
+//  Created by Sounderya Ramesh Babu on 2/2/16.
+//  Copyright © 2016 CarFix. All rights reserved.
+//
+
+import UIKit
+
+class LocateVC: UIViewController {
+//    @IBAction func LocateSuccessButtonPressed(sender: AnyObject) {
+//        let alert:UIAlertController = UIAlertController(title: "Success", message: "Request Placed Successfully", preferredStyle: <#T##UIAlertControllerStyle#>.Alert)
+//        
+//        let cancel=UIAlertAction(title: "OK", style: .Cancel){action -> Void in}
+//        alert.addAction(cancel)
+//        self.presentViewController(alert, animated: true, completion: nil)
+//    }
+
+}
